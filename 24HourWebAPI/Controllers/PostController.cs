cc5ea25f08c00fefabf00b8904116a7b9c0508ed
annajoyw <arealjoyw@gmail.com>
2021-02-06 18:29:54 -0500

    controller and more service

--- 24HourWebAPI/Controllers/PostController.cs
+++ 24HourWebAPI/Controllers/PostController.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using Microsoft.AspNet.Identity;
+using ProjectModels;
+using ProjectService;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Net;
@@ -7,7 +10,38 @@ using System.Web.Http;
 
 namespace _24HourWebAPI.Controllers
 {
+    [Authorize]
     public class PostController : ApiController
     {
+        //get post
+        public IHttpActionResult Get()
+        {
+            PostService postService = CreatePostService();
+            var posts = postService.GetPost();
+            return Ok(posts);
+        }
+        //post post
+        public IHttpActionResult Post(CreatePost post)
+        {
+            if (!ModelState.IsValid)
+                return BadRequest(ModelState);
+
+            var service = CreatePostService();
+
+            if (!service.CreatePost(post))
+                return InternalServerError();
+
+            return Ok();
+        }
+
+        //create post service
+        private PostService CreatePostService()
+        {
+            var userId = Guid.Parse(User.Identity.GetUserId());
+            var postService = new PostService(userId);
+            return postService;
+        }
+        
+            
     }
 }
--- ProjectService/CommentService.cs
+++ ProjectService/CommentService.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using ProjectData;
+using ProjectModels;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,8 +10,23 @@ namespace ProjectService
 {
     public class CommentService
     {
+        private readonly int _commentId;
+
+        public CommentService(int commentId)
+        {
+            _commentId = commentId;
+        }
         //get post comments
 
         //post comment
+        public bool CreateComment(CreateComment model)
+        {
+            var entity =
+                new Comment()
+                {
+                    CommentId = _commentId,
+                    Text = model.
+                }
+        }
     }
 }

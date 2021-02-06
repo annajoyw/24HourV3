8a1b5a8e11ab36e901c94400b0206ea1c2e16c6e
DEBJ1 <14balmerd@gmail.com>
2021-02-06 14:59:31 -0500

    Our Best

--- ProjectService/PostService.cs
+++ ProjectService/PostService.cs
@@ -2,11 +2,31 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using ProjectModels;
+
 using System.Threading.Tasks;
+
 
 namespace ProjectService
 {
     public class PostService
     {
+        private readonly Guid _userId;
+
+        public PostService (Guid userId)
+	    {
+             _userId = userId;
+	    }
+
+        
+        //Create Post
+        public bool CreatePost(CreatePost model)
+        {
+
+        }
+        
+         
+        //Get POst
+        
     }
 }
